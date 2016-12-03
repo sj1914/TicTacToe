8ba74e00e1a6e28b932eaaed07ac2bcde485e53e
Summer Jones <sj1914@ic.ac.uk>
2016-12-03 15:25:29 +0000

    Created a class to represent each square of the board

--- src/game/Square.java
+++ src/game/Square.java
@@ -1,5 +1,33 @@
 package game;
 
 public class Square {
+	
+	private int x;
+	private int y;
+	private Symbol occupier;
+	
+	// initialises square with given coordinates 
+	public Square(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	//returns x coordinate 0-2
+	public int getX() {
+		return x;
+	}
+	
+	// returns y coordinate 0-2
+	public int getY() {
+		return y;
+	}
+	
+	public Symbol occupiedBy() {
+		return occupier;
+	}
+	
+	public void setOccupier(Symbol symbol) {
+		occupier = symbol;
+	}
 
 }
